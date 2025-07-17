701fdcd734e7401059d5988430ba365652f964da
SaraDrwish <it.saradarwish@gmail.com>
2025-07-17 15:26:21 +0300

    3

--- next.config.mjs
+++ next.config.mjs
@@ -6,9 +6,19 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  output: 'export',
   images: {
     unoptimized: true,
   },
+  trailingSlash: true,
+  output: 'export',
+  basePath: '/SWYR_P1',
+  assetPrefix: '/SWYR_P1/',
 }
+ 
 
 export default nextConfig
+
+
+
+
